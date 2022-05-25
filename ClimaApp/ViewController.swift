ac38b0f16233466437b9ab370ce0a7c2b2738867
ArgonzaAr <argonza_roan@comunidad.unam.mx>
2022-05-25 09:06:18 -0500

    Subiendo proyecto de Clima en swift

---
+++ ClimaApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ClimaApp
+//
+//  Created by UNAM FCA 01 on 24/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtCiudad: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    @IBAction func buscarButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func buscarGPS(_ sender: UIButton) {
+    }
+    
+}
+
